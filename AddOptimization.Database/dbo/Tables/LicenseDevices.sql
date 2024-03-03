0f4a14e02f0e633a89d21626d03edbbad7314579
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2024-03-03 19:59:50 +0530

    renamed tables

---
+++ AddOptimization.Database/dbo/Tables/LicenseDevices.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[LicenseDevices] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [CustomerId]      UNIQUEIDENTIFIER NOT NULL,
+    [MotherBoardId]   NVARCHAR (MAX)   NULL,
+    [MachineName]     NVARCHAR (MAX)   NULL,
+    [LicenseId]       UNIQUEIDENTIFIER NOT NULL,
+    [CreatedAt]       DATETIME2 (7)    NULL,
+    [CreatedByUserId] INT              NULL,
+    [UpdatedAt]       DATETIME2 (7)    NULL,
+    [UpdatedByUserId] INT              NULL,
+    CONSTRAINT [PK_LicenseDevices] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_LicenseDevices_ApplicationUsers_CreatedByUserId] FOREIGN KEY ([CreatedByUserId]) REFERENCES [dbo].[ApplicationUsers] ([Id]),
+    CONSTRAINT [FK_LicenseDevices_ApplicationUsers_UpdatedByUserId] FOREIGN KEY ([UpdatedByUserId]) REFERENCES [dbo].[ApplicationUsers] ([Id]),
+    CONSTRAINT [FK_LicenseDevices_Licenses_LicenseId] FOREIGN KEY ([LicenseId]) REFERENCES [dbo].[Licenses] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_LicenseDevices_UpdatedByUserId]
+    ON [dbo].[LicenseDevices]([UpdatedByUserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_LicenseDevices_LicenseId]
+    ON [dbo].[LicenseDevices]([LicenseId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_LicenseDevices_CreatedByUserId]
+    ON [dbo].[LicenseDevices]([CreatedByUserId] ASC);
+
---
+++ AddOptimization.Database/dbo/Tables/Licenses.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE [dbo].[Licenses] (
+    [Id]              UNIQUEIDENTIFIER NOT NULL,
+    [LicenseKey]      NVARCHAR (255)   NULL,
+    [LicenseDuration] INT              NOT NULL,
+    [NoOfDevices]     INT              NOT NULL,
+    [ExpirationDate]  DATETIME2 (7)    NOT NULL,
+    [CustomerId]      UNIQUEIDENTIFIER NOT NULL,
+    [CreatedAt]       DATETIME2 (7)    NULL,
+    [CreatedByUserId] INT              NULL,
+    [UpdatedAt]       DATETIME2 (7)    NULL,
+    [UpdatedByUserId] INT              NULL,
+    CONSTRAINT [PK_Licenses] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Licenses_ApplicationUsers_CreatedByUserId] FOREIGN KEY ([CreatedByUserId]) REFERENCES [dbo].[ApplicationUsers] ([Id]),
+    CONSTRAINT [FK_Licenses_ApplicationUsers_UpdatedByUserId] FOREIGN KEY ([UpdatedByUserId]) REFERENCES [dbo].[ApplicationUsers] ([Id]),
+    CONSTRAINT [FK_Licenses_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Licenses_UpdatedByUserId]
+    ON [dbo].[Licenses]([UpdatedByUserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Licenses_CustomerId]
+    ON [dbo].[Licenses]([CustomerId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Licenses_CreatedByUserId]
+    ON [dbo].[Licenses]([CreatedByUserId] ASC);
+
