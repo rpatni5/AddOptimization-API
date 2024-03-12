66338b0e7ea5bf9b75967201b73e0594a1c5f095
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2024-03-12 22:03:10 +0530

    No of devices validation

--- AddOptimization.Contracts/Dto/LicenseCreateDto.cs
+++ AddOptimization.Contracts/Dto/LicenseCreateDto.cs
@@ -1,12 +1,14 @@
 ﻿using AddOptimization.Utilities.Enums;
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace AddOptimization.Contracts.Dto
 {
     public class LicenseCreateDto
     {
+        [Required]
         public Guid CustomerId { get; set; }
         public LicenseDuration LicenseDuration { get; set; }
-        public int NoOfDevices { get; set; } = 1;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Number of devices cannot be {1}")]
+        public int NoOfDevices { get; set; } = 2;
     }
 }
