03c1ff3a35a19c2f57a6b1061ba7863cf94cb163
rpatni5 <rpatni5@gmail.com>
2024-03-29 09:54:17 +0530

    License service changes

--- AddOptimization.Services/Services/LicenseService.cs
+++ AddOptimization.Services/Services/LicenseService.cs
@@ -45,10 +45,10 @@ public class LicenseService : ILicenseService
     {
         try
         {
-            var entities = await _licenseRepository.QueryAsync(include: source => source.Include(o => o.LicenseDevices).Include(o => o.Customer).Include(e => e.CreatedByUser), ignoreGlobalFilter: true);
             var superAdminRole = _currentUserRoles.Where(c => c.Contains("Super Admin")).ToList();
-            //var entities = await _licenseRepository.QueryAsync(include: source => source.Include(o => o.LicenseDevices).Include(o => o.Customer), ignoreGlobalFilter: superAdminRole.Count != 0);
-//>>>>>>> 0bfeb243875bba302056e36a954fc1b70586a202
+
+            var entities = await _licenseRepository.QueryAsync(include: source => source.Include(o => o.LicenseDevices).Include(o => o.Customer).Include(e => e.CreatedByUser), ignoreGlobalFilter: superAdminRole.Count != 0);
+
             entities = ApplySorting(entities, filter?.Sorted?.FirstOrDefault());
             entities = ApplyFilters(entities, filter);
             var pagedResult = PageHelper<License, LicenseDetailsDto>.ApplyPaging(entities, filter, entities => entities.Select(e => new LicenseDetailsDto
