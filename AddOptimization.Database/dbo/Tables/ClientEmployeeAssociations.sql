9b5235856ab85cee70bb0a2cee2f3670f91670e9
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2024-05-11 11:54:09 +0530

    Added new table ClientEmployeeAssociation

---
+++ AddOptimization.Database/dbo/Tables/ClientEmployeeAssociations.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [dbo].[ClientEmployeeAssociations](
+	[Id] [uniqueidentifier] NOT NULL,
+	[ClientId] [uniqueidentifier] NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+	[ApproverId] [int] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedAt] [datetime2](7) NULL,
+	[CreatedByUserId] [int] NULL,
+	[UpdatedAt] [datetime2](7) NULL,
+	[UpdatedByUserId] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO 
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations] ADD  DEFAULT ((0)) FOR [IsDeleted]
+GO
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations] ADD  DEFAULT ((1)) FOR [IsActive]
+GO 
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations]  WITH CHECK ADD FOREIGN KEY([ApproverId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations]  WITH CHECK ADD FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Clients] ([Id])
+GO
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations]  WITH CHECK ADD FOREIGN KEY([CreatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations]  WITH CHECK ADD FOREIGN KEY([UpdatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+ALTER TABLE [dbo].[ClientEmployeeAssociations]  WITH CHECK ADD FOREIGN KEY([EmployeeId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO 
