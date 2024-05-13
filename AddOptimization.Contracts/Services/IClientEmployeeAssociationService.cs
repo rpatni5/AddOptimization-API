1b764fa296a0cc57ceddabe2004030a41ff70358
SHIWANI JHA <132471151+shivijhaaa@users.noreply.github.com>
2024-05-13 14:50:39 +0530

    api for clientemployeeassociation add search and delete

---
+++ AddOptimization.Contracts/Services/IClientEmployeeAssociationService.cs
@@ -0,0 +1,17 @@
+﻿using AddOptimization.Contracts.Dto;
+using AddOptimization.Utilities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOptimization.Contracts.Services
+{
+    public interface IClientEmployeeAssociationService
+    {
+        Task<ApiResult<ClientEmployeeAssociationDto>> Create(ClientEmployeeAssociationDto model);
+        Task<ApiResult<List<ClientEmployeeAssociationDto>>> Search();
+        Task<ApiResult<bool>> Delete(Guid id);
+       }
+}
