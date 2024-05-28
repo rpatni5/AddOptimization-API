4332d5ebbff9b0b73c0ec74180ccce7d7b53186c
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2024-05-28 10:33:37 +0530

    Initial changes for Invoices

---
+++ AddOptimization.Database/dbo/Tables/Invoices.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE [dbo].[Invoices](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[InvoiceNumber] [bigint] NOT NULL,
+	[InvoiceDate] [datetime2](7) NOT NULL,
+	[InvoiceStatusId] [uniqueidentifier] NOT NULL,
+	[PaymentStatusId] [uniqueidentifier] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[CreatedAt] [datetime2](7) NULL,
+	[CreatedByUserId] [int] NULL,
+	[UpdatedAt] [datetime2](7) NULL,
+	[UpdatedByUserId] [int] NULL,
+	[CustomerId] [uniqueidentifier] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO 
+ALTER TABLE [dbo].[Invoices] ADD  DEFAULT ((0)) FOR [IsDeleted]
+GO 
+
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD FOREIGN KEY([PaymentStatusId])
+REFERENCES [dbo].[PaymentStatuses] ([Id])
+GO
+
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD FOREIGN KEY([InvoiceStatusId])
+REFERENCES [dbo].[InvoiceStatuses] ([Id])
+GO
+
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([Id])
+GO
+
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD FOREIGN KEY([CreatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD FOREIGN KEY([UpdatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
