1439cbff8b11fc4f6f03e84a6430f17c41f3f484
SHIWANI JHA <132471151+shivijhaaa@users.noreply.github.com>
2024-06-02 14:12:17 +0530

    change

--- AddOptimization.Data/Entities/Company.cs
+++ AddOptimization.Data/Entities/Company.cs
@@ -35,7 +35,7 @@ public class Company : BaseEntityNew<Guid>
     public string Street { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
-    public int ZipCode { get; set; }
+    public int? ZipCode { get; set; }
 
 
 }
