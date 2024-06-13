8b43c2d3de370aec68ba92908eb308c7997bdc92
rpatni5 <rpatni5@gmail.com>
2024-06-13 12:43:32 +0530

    quote generation company detail changes

--- AddOptimization.Contracts/Dto/CompanyDto.cs
+++ AddOptimization.Contracts/Dto/CompanyDto.cs
@@ -20,6 +20,7 @@ namespace AddOptimization.Contracts.Dto
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+        public string? CountryName { get; set; }
         public int? ZipCode { get; set; }
         public string? SwiftCode { get; set; }
         public string? State { get; set; }
