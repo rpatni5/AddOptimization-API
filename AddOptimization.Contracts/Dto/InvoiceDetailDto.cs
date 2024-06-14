60349792df2163f2ba8e2269efead26d2129fbc9
SHIWANI JHA <132471151+shivijhaaa@users.noreply.github.com>
2024-06-14 16:28:06 +0530

    save invoice

---
+++ AddOptimization.Contracts/Dto/InvoiceDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AddOptimization.Contracts.Dto
+{
+    public class InvoiceDetailDto
+    {
+        public int InvoiceId { get; set; }
+        public string Description { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal VatPercent { get; set; }
+        public decimal TotalPriceIncludingVat { get; set; }
+        public decimal TotalPriceExcludingVat { get; set; }
+        public string Metadata { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
