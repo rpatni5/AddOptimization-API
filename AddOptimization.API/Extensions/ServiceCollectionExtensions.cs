c8ec1a517575f81c140b896d309cdc676b6427ce
Vivek Singh Negi <viveksinghnegi7@gmail.com>
2024-07-05 21:48:15 +0530

    Implemented settings

--- AddOptimization.API/Extensions/ServiceCollectionExtensions.cs
+++ AddOptimization.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,11 @@
-﻿namespace AddOptimization.API.Extensions;
+﻿using AddOptimization.Contracts.Constants;
+using AddOptimization.Contracts.Services;
+
+namespace AddOptimization.API.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddCors(this IServiceCollection services, WebApplicationBuilder builder,string allowOrigin)
+    public static void AddCors(this IServiceCollection services, WebApplicationBuilder builder, string allowOrigin)
     {
         services.AddCors(options =>
         {
@@ -10,6 +13,30 @@ public static class ServiceCollectionExtensions
             {
                 policy.WithOrigins(builder.Configuration["Cors:AllowOrigins"]).AllowAnyHeader().AllowAnyMethod();
             });
+        });
+    }
+
+    public static void AddLoggingService(this IServiceCollection services)
+    {
+        services.AddLogging(builder =>
+        {
+
+            var a = builder.Services.BuildServiceProvider();
+            using (var scope = a.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var logLevelService = services.GetRequiredService<ISettingService>();
+                    var logLevelSetting = logLevelService.GetSettingByCode(SettingCodes.LOG_LEVEL).Result;
+                    builder.AddFile($"logs/log_for.txt", logLevelSetting.Result!=null && logLevelSetting.Result.IsEnabled ? LogLevel.Information : LogLevel.Error);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while setting the log level.");
+                }
+            }
         });
     }
 }
---
+++ AddOptimization.Contracts/Constants/SettingCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace AddOptimization.Contracts.Constants;
+
+public class SettingCodes
+{
+    public const string EMAIL_NOTIFICATIONS = "EMAIL_NOTIFICATIONS";
+    public const string SSO_LOGIN = "SSO_LOGIN";
+    public const string LOG_LEVEL = "LOG_LEVEL";
+}
