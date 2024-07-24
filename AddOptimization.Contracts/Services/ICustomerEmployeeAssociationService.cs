0ef1e2029fe9f64ad8241430d72f2f20c1d7594f
shivi <shivi@LAPTOP-C6FJNGFG>
2024-07-24 13:38:30 +0530

    server side filtering in the employee association api

--- AddOptimization.Contracts/Services/ICustomerEmployeeAssociationService.cs
+++ AddOptimization.Contracts/Services/ICustomerEmployeeAssociationService.cs
@@ -1,5 +1,6 @@
 ﻿using AddOptimization.Contracts.Dto;
 using AddOptimization.Utilities.Common;
+using AddOptimization.Utilities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace AddOptimization.Contracts.Services
     {
         Task<ApiResult<CustomerEmployeeAssociationDto>> Create(CustomerEmployeeAssociationDto model);
         Task<ApiResult<List<CustomerEmployeeAssociationDto>>> Search();
+        Task<PagedApiResult<CustomerEmployeeAssociationDto>> SearchAllAssociations(PageQueryFiterBase filter);
         Task<ApiResult<bool>> Delete(Guid id);
         Task<ApiResult<List<CustomerEmployeeAssociationDto>>> GetAssociatedCustomers(int employeeId);
         Task<ApiResult<CustomerEmployeeAssociationDto>> Get(Guid id);
