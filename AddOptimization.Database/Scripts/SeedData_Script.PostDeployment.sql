9db664fa9f35b6b5944d6815d9d642be4c7b670a
Megha <megharana279@gmail.com>
2024-09-04 18:20:11 +0530

    FieldKey  added

--- AddOptimization.Database/Scripts/SeedData_Script.PostDeployment.sql
+++ AddOptimization.Database/Scripts/SeedData_Script.PostDeployment.sql
@@ -715,18 +715,18 @@ END
 
 /*Adding NIE Number in Employee Identity */
 
-IF NOT EXISTS (SELECT 1 FROM EmployeeIdentity Where FieldKey= 'nie number')
+IF NOT EXISTS (SELECT 1 FROM EmployeeIdentity Where FieldKey= 'nie_number')
 BEGIN 
     INSERT INTO EmployeeIdentity(Id,Name,FieldKey)
     VALUES
-        (NEWID(),'NIE Number', 'nie number')
+        (NEWID(),'NIE Number', 'nie_number')
 END
 
 /*Adding Passport Number in Employee Identity*/
 
-IF NOT EXISTS (SELECT 1 FROM EmployeeIdentity Where FieldKey= 'passport number')
+IF NOT EXISTS (SELECT 1 FROM EmployeeIdentity Where FieldKey= 'passport_number')
 BEGIN 
     INSERT INTO EmployeeIdentity(Id,Name,FieldKey)
     VALUES
-        (NEWID(),'Passport Number','passport number')
+        (NEWID(),'Passport Number','passport_number')
 END
