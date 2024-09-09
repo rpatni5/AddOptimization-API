e0e5787b87d58d0711e0e96df2be0f1d88cee9b5
shivi <shivi@LAPTOP-C6FJNGFG>
2024-09-09 15:30:34 +0530

    absence req added two column start and end date and removed date column

--- AddOptimization.Database/dbo/Tables/AbsenceRequest.sql
+++ AddOptimization.Database/dbo/Tables/AbsenceRequest.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE [dbo].[AbsenceRequest](
+﻿
+CREATE TABLE [dbo].[AbsenceRequest](
 	[Id] [uniqueidentifier] NOT NULL,
 	[Comment] [nvarchar](500) NULL,
-	[Date] [datetime] NOT NULL,
 	[CreatedAt] [datetime2](7) NOT NULL,
 	[CreatedByUserId] [int] NOT NULL,
 	[UpdatedAt] [datetime2](7) NULL,
@@ -10,9 +10,10 @@
 	[IsActive] [bit] NOT NULL,
 	[UserId] [int] NOT NULL,
 	[LeaveStatusId] [int] NOT NULL,
-	[Duration] [decimal](10, 2) NOT NULL
+	[Duration] [decimal](10, 2) NOT NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL
 ) ON [PRIMARY]
-
 GO
 
 ALTER TABLE [dbo].[AbsenceRequest] ADD  DEFAULT ((0)) FOR [IsDeleted]
