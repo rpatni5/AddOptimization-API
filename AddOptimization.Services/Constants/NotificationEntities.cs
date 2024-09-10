c5a732418f0ce9bbf064ee573a1d35a610f9b53a
rpatni5 <rpatni5@gmail.com>
2024-09-10 16:32:47 +0530

    Added base class for notifications

---
+++ AddOptimization.Services/Constants/NotificationEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOptimization.Services.Constants
+{
+    public class NotificationEntities
+    {
+        public const string Quote = "Quote";
+        public const string Invoice = "Invoice";
+    }
+}
