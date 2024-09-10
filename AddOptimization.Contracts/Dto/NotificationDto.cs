9477e6e04377f6d5060aafb84a1631d395681b75
rpatni5 <rpatni5@gmail.com>
2024-09-10 16:07:16 +0530

    Intofication system added base classes

---
+++ AddOptimization.Contracts/Dto/NotificationDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOptimization.Contracts.Dto
+{
+    public class NotificationDto
+    {
+        public int? Id { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string Link { get; set; }
+        public string GroupKey { get; set; }
+        public string Meta { get; set; }
+        public bool IsRead { get; set; }
+        public int? AppplicationUserId { get; set; }
+        public NotificationUserDto CreatedByUser { get; set; }
+        public DateTime? ReadAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+
+    }
+    public class NotificationUserDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AddOptimization.Data/Entities/Notification.cs
@@ -0,0 +1,34 @@
+﻿using AddOptimization.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOptimization.Data.Entities
+{
+    public class Notification : BaseEntityNewCreatedOnlyProps<int>
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        [MaxLength(300)]
+        public string Link { get; set; }
+        public string GroupKey { get; set; }
+        public string Meta { get; set; }
+
+        public bool? IsRead { get; set; }
+
+        public int? AppplicationUserId { get; set; }
+
+        public DateTime? ReadAt { get; set; }
+
+        [ForeignKey("AppplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
