88e2b682c9be91ac4782faa588ec7615196dc17f
shivi <shivi@LAPTOP-C6FJNGFG>
2024-09-10 18:19:50 +0530

    invoice changes in database

--- AddOptimization.Database/dbo/Tables/Invoices.sql
+++ AddOptimization.Database/dbo/Tables/Invoices.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Invoices](
 	[Id] [bigint] NOT NULL,
-	[InvoiceNumber] [bigint] NOT NULL,
+	[InvoiceNumber] [varchar](250) NULL,
 	[InvoiceDate] [datetime2](7) NOT NULL,
 	[InvoiceStatusId] [uniqueidentifier] NOT NULL,
 	[PaymentStatusId] [uniqueidentifier] NOT NULL,
@@ -22,6 +22,7 @@
 	[Metadata] [varchar](max) NULL,
 	[HasCreditNotes] [bit] NOT NULL,
 	[CreditNoteNumber] [bigint] NULL,
+	[HasInvoiceFinalized] [bit] NULL,
  CONSTRAINT [PK_Invoices] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -33,6 +34,9 @@ ALTER TABLE [dbo].[Invoices] ADD  CONSTRAINT [DF__Invoices__IsDele__5006DFF2]  D
 GO
 
 ALTER TABLE [dbo].[Invoices] ADD  DEFAULT ((0)) FOR [HasCreditNotes]
+GO
+
+ALTER TABLE [dbo].[Invoices] ADD  DEFAULT ((0)) FOR [HasInvoiceFinalized]
 GO
 
 ALTER TABLE [dbo].[Invoices]  WITH CHECK ADD  CONSTRAINT [FK_Invoices_ApplicationUsers_CreatedByUserId] FOREIGN KEY([CreatedByUserId])
