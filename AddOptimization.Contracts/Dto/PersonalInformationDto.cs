d5c868dd0375b0cec981bf74f14e4a203b095852
Sanjay <sanjaychandola3535@gmail.com>
2024-10-03 16:15:35 +0530

    changed to string

--- AddOptimization.Contracts/Dto/PersonalInformationDto.cs
+++ AddOptimization.Contracts/Dto/PersonalInformationDto.cs
@@ -13,7 +13,7 @@ namespace AddOptimization.Contracts.Dto
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public Guid Country { get; set; }
+        public string Country { get; set; }
         public string Address { get; set; }
         public string Address1 { get; set; }
         public string City { get; set; }
