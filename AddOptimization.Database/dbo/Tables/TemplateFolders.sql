c2b6147b6cfe61baf35557fc83f8c7db616ca5dd
shivi <shivi@LAPTOP-C6FJNGFG>
2024-10-07 12:30:59 +0530

    script added for password vault screens

---
+++ AddOptimization.Database/dbo/Tables/TemplateFolders.sql
@@ -0,0 +1,29 @@
+﻿
+CREATE TABLE [dbo].[TemplateFolders](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[CreatedAt] [datetime2](7) NULL,
+	[CreatedByUserId] [int] NULL,
+	[UpdatedAt] [datetime2](7) NULL,
+	[UpdatedByUserId] [int] NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[Description] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TemplateFolders] ADD  DEFAULT ((0)) FOR [IsDeleted]
+GO
+
+ALTER TABLE [dbo].[TemplateFolders]  WITH CHECK ADD FOREIGN KEY([CreatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+ALTER TABLE [dbo].[TemplateFolders]  WITH CHECK ADD FOREIGN KEY([UpdatedByUserId])
+REFERENCES [dbo].[ApplicationUsers] ([Id])
+GO
+
+
