35deb32c7105a1676930a8f8f29733405bf29342
Sanjay <sanjaychandola3535@gmail.com>
2024-10-08 18:51:57 +0530

    mobile application api completed and templatekey added in template service

---
+++ AddOptimization.Contracts/Services/IMobileApplicationService.cs
@@ -0,0 +1,20 @@
+﻿using AddOptimization.Contracts.Dto;
+using AddOptimization.Utilities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOptimization.Contracts.Services
+{
+    public interface IMobileApplicationService
+    {
+
+        Task<ApiResult<bool>> SaveMobileAppDetails(TemplateEntryDto model);
+        Task<ApiResult<List<TemplateEntryDto>>> Search();
+        Task<ApiResult<TemplateEntryDto>> Update(Guid id, TemplateEntryDto model);
+        Task<ApiResult<bool>> Delete(Guid id);
+        Task<ApiResult<TemplateEntryDto>> GetById(Guid id);
+    }
+}
