d0232872ecdb0805ef3a6a6bf734f533b1865421
Sanjay <sanjaychandola3535@gmail.com>
2024-11-05 17:49:52 +0530

    sql  queries

---
+++ AddOptimization.Database/dbo/Tables/CvEntryHistory.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[CvEntryHistory](
+	[Id] [uniqueidentifier] NOT NULL,
+	[CVEntryId] [uniqueidentifier] NOT NULL,
+	[EntryData] [nvarchar](max) NULL,
+	[CreatedAt] [datetime2](7) NULL,
+	[CreatedByUserId] [int] NULL,
+	[UpdatedAt] [datetime2](7) NULL,
+	[UpdatedByUserId] [int] NULL,
+	[IsDeleted] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[CvEntryHistory]  WITH CHECK ADD FOREIGN KEY([CVEntryId])
+REFERENCES [dbo].[CvEntries] ([Id])
+GO
+
+
+
+
